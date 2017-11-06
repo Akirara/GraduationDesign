a5990041283933d7a6341d2fb40d6e864ce4c407
Akirara <zhaoxvan@gmail.com>
2017-11-06 13:54:14 +0800

    add paper

--- code/station_multi.py
+++ code/station_multi.py
@@ -5,9 +5,38 @@ import xgboost as xgb
 from sklearn.model_selection import KFold
 from sklearn.neural_network import MLPClassifier
 
+"""
+    Station multi classification.
+"""
+
 
 def run():
-    pass
+    """
+    neural network model
+    """
+    mlpc = MLPClassifier(
+        hidden_layer_sizes=(100,),
+        activation='relu',
+        solver='adam',
+        alpha=0.0001,
+        batch_size='auto',
+        learning_rate='constant',
+        learning_rate_init=0.001,
+        power_t=0.5,
+        max_iter=200,
+        shuffle=True,
+        random_state=None,
+        tol=0.0001,
+        verbose=False,
+        warm_start=False,
+        momentum=0.9,
+        nesterovs_momentum=True,
+        early_stopping=False,
+        validation_fraction=0.1,
+        beta_1=0.1,
+        beta_2=0.999,
+        epsilon=1e-08
+    );
 
 
 if __name__ == "__main__":
